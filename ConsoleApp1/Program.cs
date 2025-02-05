c1acff692d1bffa632d158fcb8fb81e5eee75fec
MyaMya0709 <chlehdgh96@gmail.com>
2025-02-05 15:09:41 +0900

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character player1 = new Character("철수");
+            Character player2 = new Character("영희");
+            ShowName(player1);
+            ShowName(player2);
+            GiveGold(player1, 50);
+            GiveGold(player2, 50);
+        }
+        static void ShowName(Character target)
+        {
+            Console.WriteLine(player1.myName); // 틀림
+            Console.WriteLine(target.myName);
+        }
+        static void GiveGold(Character target, int amt)
+        {
+            target.gold += amt;
+            if (target.gold < 0) target.gold = 0;
+        }
+    }
+    internal class Character
+    {
+        public string myName;
+        public int gold;
+        public Character(string name)
+        {
+            myName = name;
+        }
+    }
+}
---
+++ TextRPG_SpartaDungeon/Program.cs
@@ -0,0 +1,309 @@
+﻿using System.Xml.Linq;
+using Microsoft.Win32.SafeHandles;
+using System.Collections.Generic;
+
+namespace TextRPG_SpartaDungeon
+{
+    public interface ICharacter
+    {
+        string Name { get; }
+        int Health { get; set; }
+        int Attack { get; }
+    }
+
+    //플레이어 클래스
+    public class Player : ICharacter
+    {
+        public int Lv { get; set; }
+        public string Name { get;}
+        public string Job { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int Health { get; set; }
+        public int Gold { get; set; }
+
+        public Player(string name)
+        {
+            this.Lv = 1;
+            this.Name = name;
+            this.Job = "전사";
+            this.Attack = 10;
+            this.Defence = 5;
+            this.Health = 100;
+            this.Gold = 1500;
+        }
+    }
+
+    public interface Item
+    {
+        string Name { get; }
+        string Effect { get; }
+        string Description { get; }
+        void Equip(Player warrior); // 전사에게 아이템을 사용하는 메서드
+    }
+
+    ////아이템
+    class Sword : Item
+    {
+        public string Name => "낡은 검";
+        public string Effect => "공격력 + 2";
+        public string Description => "쉽게 볼 수 있는 낡은 검 입니다.";
+        public void Equip(Player warrior)
+        {
+            Console.WriteLine($"{Name}을 장착합니다.");
+            warrior.Attack += 2;
+        }
+    }
+    class Spear : Item
+    {
+        public string Name => "스파르타의 창";
+        public string Effect => "공격력 + 7";
+        public string Description => "스파르타의 전사들이 사용했다는 전설의 창입니다.";
+        public void Equip(Player warrior)
+        {
+            Console.WriteLine($"{Name}을 장착합니다.");
+            warrior.Attack += 7;
+        }
+    }
+    class Armor : Item
+    {
+        public string Name => "무쇠갑옷";
+        public string Effect => "방어력 + 5";
+        public string Description => "무쇠로 만들어져 튼튼한 갑옷입니다.";
+        public void Equip(Player warrior)
+        {
+            Console.WriteLine($"{Name}을 장착합니다.");
+            warrior.Defence += 5;
+        }
+    }
+
+    //인벤토리
+    class Inventory
+    {
+        public Sword sword = new Sword();
+        public Spear spear = new Spear();
+        public Armor armor = new Armor();
+
+        public List<Item> items = new List<Item>();
+        
+        public Inventory()
+        {
+            items.Add( sword );
+            items.Add( spear );
+            items.Add( armor );
+        }
+    }
+
+    //게임시작 클래스
+    class GameManager
+    {
+        public Player player;
+        public Inventory inventory;
+        public GameManager()
+        {
+            Console.WriteLine("이름을 입력해주세요!");
+            player = new Player(Console.ReadLine());
+            inventory = new Inventory();
+        }
+
+        //시작화면 마을화면
+        public void ShowStartScene()
+        {
+            Console.Clear();
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Console.WriteLine("\n1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.\n>>");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int sellecNumber;
+                bool isNumber = int.TryParse(input, out sellecNumber);
+
+                switch (sellecNumber)
+                {
+                    case 1:
+                        StatusDisPlay(player);
+                        break;
+
+                    case 2:
+                        InvenDisPlay(inventory);
+                        break;
+
+                    case 3:
+                        StoreDisplay(player, inventory);
+                        break;
+
+                    default:
+                        Console.WriteLine("잘못된 입력입니다");
+                        break;
+                }
+            }
+        }
+
+        //상태창
+        public void StatusDisPlay(Player targetplayer)
+        {
+            Console.Clear();
+            Console.WriteLine("상태보기\n캐릭터의 정보가 표시됩니다.");
+            Console.WriteLine($"\nLv. {targetplayer.Lv}");
+            Console.WriteLine($"{targetplayer.Name} ({targetplayer.Job})");
+            Console.WriteLine($"공격력 : {targetplayer.Attack}");
+            Console.WriteLine($"방어력 : {targetplayer.Defence}");
+            Console.WriteLine($"체력 : {targetplayer.Health}");
+            Console.WriteLine($"Gold : {targetplayer.Gold}");
+            Console.WriteLine("\n0. 나가기\n");
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.\n>>");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int sellecNumber;
+                bool isNumber = int.TryParse(input, out sellecNumber);
+
+                switch (sellecNumber)
+                {
+                    case 0:
+                        ShowStartScene();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("잘못된 입력입니다");
+                        break;
+
+                }
+            }
+        }
+
+        //인벤토리 창
+        public void InvenDisPlay(Inventory itemlist)
+        {
+            
+            Console.Clear();
+            Console.WriteLine("인벤토리\n보유 중인 아이템을 관리할 수 있습니다.\n");
+            Console.WriteLine("\n[아이템 목록]");
+            foreach (Item item in itemlist.items)
+            {
+                Console.WriteLine($"{item.Name}|{item.Effect}|{item.Description}");
+            }
+            Console.WriteLine("\n1. 장착관리");
+            Console.WriteLine("0. 나가기\n");
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.\n>>");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int sellecNumber;
+                bool isNumber = int.TryParse(input, out sellecNumber);
+
+
+                switch (sellecNumber)
+                {
+                    case 0:
+                        ShowStartScene();
+                        break;
+                    case 1:
+                        EquipDisPlay(inventory);
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다");
+                        break;
+
+                }
+            }
+            
+        }
+
+        //장착 관리 창
+        public void EquipDisPlay(Inventory itemlist)
+        {
+
+            Console.Clear();
+            Console.WriteLine("인벤토리 - 장착 관리\n보유 중인 아이템을 관리할 수 있습니다.\n");
+            Console.WriteLine("\n[아이템 목록]");
+            foreach (Item item in itemlist.items)
+            {
+                int i =1;
+                i++;
+                Console.WriteLine($"- {i} {item.Name}|{item.Effect}|{item.Description}");
+            }
+            Console.WriteLine("\n0. 나가기\n");
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.\n>>");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int sellecNumber;
+                bool isNumber = int.TryParse(input, out sellecNumber);
+
+
+                switch (sellecNumber)
+                {
+                    case 0:
+                        ShowStartScene();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("잘못된 입력입니다");
+                        break;
+
+                }
+            }
+
+        }
+
+        // 상점 창
+        public void StoreDisplay(Player targetplayer, Inventory itemlist)
+        {
+            Console.Clear();
+            Console.WriteLine("상점\n필요한 아이템을 얻을 수 있는 상점입니다.\n");
+           
+            Console.WriteLine("\n[보유골드]");
+            Console.WriteLine($"\n{targetplayer.Gold} G");
+
+            Console.WriteLine("\n[아이템 목록]");
+            foreach (Item item in itemlist.items)
+            {
+                Console.WriteLine($"{item.Name}|{item.Effect}|{item.Description}");
+            }
+
+            Console.WriteLine("\n1. 아이탬 구매");
+            Console.WriteLine("0. 나가기\n");
+
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.\n>>");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int sellecNumber;
+                bool isNumber = int.TryParse(input, out sellecNumber);
+
+
+                switch (sellecNumber)
+                {
+                    case 0:
+                        ShowStartScene();
+                        break;
+                    case 1:
+                        //아이템 구매창
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다");
+                        break;
+
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager Gm = new GameManager();
+            Gm.ShowStartScene();
+        }
+    }
+}
